c4ebc209c13ed8bba231c1325fb053eed3a2bd3f
Kirill Zaidulin <zaidulinkirill@gmail.com>
2023-10-11 00:00:56 +0300

    export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { Contacts } from './Contacts';
 
 export * from './Contacts';
+export * from './types';
 
 export const fetchContacts = Contacts.fetchContacts;
 export const updateContacts = Contacts.updateContacts;
